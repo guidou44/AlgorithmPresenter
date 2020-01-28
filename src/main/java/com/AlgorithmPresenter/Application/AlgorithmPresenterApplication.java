cf0d11fde36d1fa806169ac701ee00b0d6b7c0f1
guidou44 <guim_44@hotmail.com>
2020-01-28 07:06:19 -0500

    fixed the problem when switching intelliJ version

--- src/main/java/com/AlgorithmPresenter/Application/AlgorithmPresenterApplication.java
+++ src/main/java/com/AlgorithmPresenter/Application/AlgorithmPresenterApplication.java
@@ -1,16 +1,15 @@
 package com.AlgorithmPresenter.Application;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class AlgorithmPresenterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AlgorithmPresenterApplication.class, args);
+		run(AlgorithmPresenterApplication.class, args);
 	}
 
 }
--- src/main/java/com/AlgorithmPresenter/Application/Controllers/HelloWorldController.java
+++ src/main/java/com/AlgorithmPresenter/Application/Controllers/HelloWorldController.java
@@ -1,9 +1,17 @@
 package com.AlgorithmPresenter.Application.Controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.time.LocalTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.springframework.http.MediaType.*;
 
 
 @Controller
@@ -15,5 +23,49 @@ public class HelloWorldController extends ControllerBase {
         model.addAttribute("name", name); //makes available for view
         return getMainViewName();
     }
+
+
+    //region Sample SSE
+
+    @GetMapping(path = "/comment/stream", produces = TEXT_EVENT_STREAM_VALUE)
+    public ResponseBodyEmitter handleRequest () {
+
+        final SseEmitter emitter = new SseEmitter();
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.execute(() -> {
+            for (int i = 0; i < 500; i++) {
+                try {
+                    emitter.send(LocalTime.now().toString());
+
+                    Thread.sleep(200);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    emitter.completeWithError(e);
+                    return;
+                }
+            }
+            emitter.complete();
+        });
+
+        return emitter;
+    }
+
+//    @GetMapping(value = "/notifyonEvent", produces =
+//            MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public Flux<String> getData() {
+//        Random r = new Random();
+//        int low = 0;
+//        int high = 50;
+//        return Flux.fromStream(Stream.generate(() -> r.nextInt(high - low) + low)
+//                .map(s -> String.valueOf(s))
+//                .peek((msg) -> {
+//                    LOGGER.info(msg);
+//                }))
+//                .map(s -> s)
+//                .delayElements(Duration.ofSeconds(1));
+//    }
+//
+
+    //endregion
 
 }
