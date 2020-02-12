a64d3aae2c6f4067753fc0e89f0706e934bc6a16
guidou44 <guim_44@hotmail.com>
2020-02-12 18:46:12 -0500

    added gif for final button in sorting ui

--- src/main/resources/static/js/CollectionManager.js
+++ src/main/resources/static/js/CollectionManager.js
@@ -74,3 +74,17 @@ function incrementCollectionSize() {
   sizeInput.value = parseInt(sizeInput.value, 10) + 1;
   getNewCollection();
 }
+
+function displayCharacterKoopaMagician(ms) {
+  var divContainingCharacter = document.getElementById("CharacterContainer");
+  console.log("setting visibility visible");
+  divContainingCharacter.style.visibility = "visible";
+  sleep(ms).then(() => {
+    divContainingCharacter.style.visibility = "hidden";
+    console.log("setting visibility hidden");
+  });
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
